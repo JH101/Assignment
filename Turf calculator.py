b2d6d4f0b7df3db766f0ef1043662097331309c3
JH101 <41425@longroad.ac.uk>
2014-09-23 11:36:56 +0100

    Work done 23/09/14

---
+++ Turf calculator.py
@@ -0,0 +1,8 @@
+#Jamie Hilton
+#19/09/14
+#Turf Calculator v1.1
+
+length = float(input("Please enter the length of your garden in m: "))
+
+width = float(input("Please enter the width of your garden in m: "))
+
